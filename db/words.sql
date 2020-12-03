a6b605fcc45466e0bae9cca3e1bdf71800c353fa
Willem kalverda <willem@kalverda.com>
2020-12-03 21:36:38 +0100

    Changing the database to store exchange rates

--- db/words.sql
+++ db/words.sql
@@ -1,55 +1,12 @@
-CREATE TABLE nouns (word TEXT NOT NULL);
-CREATE TABLE verbs (word TEXT NOT NULL);
-CREATE TABLE adjectives (word TEXT NOT NULL);
-
-INSERT INTO nouns(word) VALUES
-  ('cloud'),
-  ('elephant'),
-  ('gø language'),
-  ('laptøp'),
-  ('cøntainer'),
-  ('micrø-service'),
-  ('turtle'),
-  ('whale'),
-  ('gøpher'),
-  ('møby døck'),
-  ('server'),
-  ('bicycle'),
-  ('viking'),
-  ('mermaid'),
-  ('fjørd'),
-  ('legø'),
-  ('flødebolle'),
-  ('smørrebrød');
-
-INSERT INTO verbs(word) VALUES
-  ('will drink'),
-  ('smashes'),
-  ('smøkes'),
-  ('eats'),
-  ('walks tøwards'),
-  ('løves'),
-  ('helps'),
-  ('pushes'),
-  ('debugs'),
-  ('invites'),
-  ('hides'),
-  ('will ship');
-
-INSERT INTO adjectives(word) VALUES
-  ('the exquisite'),
-  ('a pink'),
-  ('the røtten'),
-  ('a red'),
-  ('the serverless'),
-  ('a brøken'),
-  ('a shiny'),
-  ('the pretty'),
-  ('the impressive'),
-  ('an awesøme'),
-  ('the famøus'),
-  ('a gigantic'),
-  ('the gløriøus'),
-  ('the nørdic'),
-  ('the welcøming'),
-  ('the deliciøus');
+CREATE TABLE eurtousd (
+  call_id int NOT NULL AUTO_INCREMENT,
+  conversion DECIMAL(2,20) NOT NULL,
+  inserted_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (call_id)
+);
+CREATE TABLE usdtoeur (
+  call_id int NOT NULL AUTO_INCREMENT,
+  conversion DECIMAL(2,20) NOT NULL,
+  inserted_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (call_id)
+);
--- words/src/main/java/Main.java
+++ words/src/main/java/Main.java
@@ -3,34 +3,84 @@ import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigDecimal;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.NoSuchElementException;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         Class.forName("org.postgresql.Driver");
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/noun", handler(Suppliers.memoize(() -> randomWord("nouns"))));
-        server.createContext("/verb", handler(Suppliers.memoize(() -> randomWord("verbs"))));
-        server.createContext("/adjective", handler(Suppliers.memoize(() -> randomWord("adjectives"))));
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+
+        server.createContext("/hello", helloHandler());
+
+        // Start the server.
         server.start();
     }
 
-    private static String randomWord(String table) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://db:5432/postgres", "postgres", "")) {
+    private static String fetchHistorical() {
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        Instant now = Instant.now();
+        Instant yesterday = now.minus(1, ChronoUnit.DAYS);
+        String today = simpleDateFormat.format(now);
+        String preceedingDay = simpleDateFormat.format(yesterday);
+       
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.exchangeratesapi.io/history?start_at=" + preceedingDay + "&end_at=" + today
+                        + "&base=EUR&symbols=USD"))
+                .build();
+        client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println)
+                .join();
+    }
+
+    /*
+     * public static void main(String[] args) throws Exception {
+     * Class.forName("org.postgresql.Driver");
+     * 
+     * HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+     * server.createContext("/noun", handler(Suppliers.memoize(() ->
+     * randomWord("nouns")))); server.createContext("/verb",
+     * handler(Suppliers.memoize(() -> randomWord("verbs"))));
+     * server.createContext("/adjective", handler(Suppliers.memoize(() ->
+     * randomWord("adjectives")))); server.start(); } //
+     */
+
+    private static String querryRates(String table) {
+        BigDecimal avarageRates = BigDecimal.ZERO;
+        int valuesFound = 0;
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://db:5432/postgres", "postgres",
+                "")) {
             try (Statement statement = connection.createStatement()) {
-                try (ResultSet set = statement.executeQuery("SELECT word FROM " + table + " ORDER BY random() LIMIT 1")) {
+                try (ResultSet set = statement.executeQuery("SELECT conversion " + // querry
+                        "FROM " + table + " " + // from either table
+                        "ORDER BY post_datetime DESC " + // fetching the latest
+                        "LIMIT 10")) { // 10 entries
                     while (set.next()) {
-                        return set.getString(1);
+                        ++valuesFound;
+                        BigDecimal conversion = set.getBigDecimal(1);
+                        avarageRates.add(conversion);
                     }
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+
+        // Only return a value if an hour's worth of averages have collected
+        if (avarageRates.compareTo(BigDecimal.ZERO) != 0 && valuesFound == 10) {
+            return avarageRates.divide(BigDecimal.TEN).subtract(BigDecimal.ONE).toPlainString();
         }
 
         throw new NoSuchElementException(table);
@@ -50,4 +100,18 @@ public class Main {
             }
         };
     }
+
+    private static HttpHandler helloHandler() {
+        return t -> {
+            byte[] response = "Hello World from Google App Engine Java 11.".getBytes();
+
+            System.out.println(response);
+
+            t.sendResponseHeaders(200, response.length);
+            try (OutputStream os = t.getResponseBody()) {
+                os.write(response);
+            }
+        };
+    }
+
 }
